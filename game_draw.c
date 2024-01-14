edda35807c08ff13bcb88d1277bdc98484c2cc08
Najat Khoudrouf <nkhoudro@e2r2p11.1337.ma>
2024-01-14 20:48:19 +0100

    fixed all

--- game_draw.c
+++ game_draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   game_draw.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saboulal <saboulal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nkhoudro <nkhoudro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 13:25:55 by nkhoudro          #+#    #+#             */
-/*   Updated: 2024/01/14 20:44:25 by saboulal         ###   ########.fr       */
+/*   Updated: 2024/01/14 20:47:44 by nkhoudro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -508,9 +508,9 @@ void key_press(mlx_key_data_t keydata, void *mlx)
 
     map = mlx;
     if ((keydata.key == MLX_KEY_S) && (keydata.action == MLX_PRESS)) // w 
-        map->player.walkDirection = 1;
-    else if (keydata.key == MLX_KEY_W && (keydata.action == MLX_PRESS))// s
         map->player.walkDirection = -1;
+    else if (keydata.key == MLX_KEY_W && (keydata.action == MLX_PRESS))// s
+        map->player.walkDirection = 1;
     else if (keydata.key == MLX_KEY_A && (keydata.action == MLX_PRESS))// A
         map->player.walkleftright = 1;
     else if (keydata.key == MLX_KEY_D && (keydata.action == MLX_PRESS))// D
