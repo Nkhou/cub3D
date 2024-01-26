5efadd1b325971d5ba511ccc17297ae15afb7c64
Salma Boulal <saboulal@e2r2p13.1337.ma>
2024-01-26 13:36:40 +0100

    norm check.c and cub3d.c is ok

--- mandatory/cub3d.c
+++ mandatory/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: saboulal <saboulal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 16:19:04 by saboulal          #+#    #+#             */
-/*   Updated: 2024/01/26 11:28:25 by saboulal         ###   ########.fr       */
+/*   Updated: 2024/01/26 13:05:36 by saboulal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,17 @@
 // {
 //    system("leaks cub3D");
 // }
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_map map;
-    
-    // atexit(ft_exit);
-    ini_map(&map,argc);
-    ft_extention(argv);
-    init_infos(&map,argv);
-    init_part_map(&map);
-    init_parse(&map);
-    get_space_wall(map);
-    map_draw(map);
-    free_programme(map.str,map);
-    return(0);
- }
- 
+	t_map	map;
 
-
-
-
+	ini_map(&map, argc);
+	ft_extention(argv);
+	init_infos(&map, argv);
+	init_part_map(&map);
+	init_parse(&map);
+	get_space_wall(map);
+	map_draw(map);
+	free_programme(map.str, map);
+	return (0);
+}
--- mandatory/parse/norm_2.c
+++ mandatory/parse/norm_2.c
@@ -6,7 +6,7 @@
 /*   By: saboulal <saboulal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 11:14:04 by saboulal          #+#    #+#             */
-/*   Updated: 2024/01/26 11:14:36 by saboulal         ###   ########.fr       */
+/*   Updated: 2024/01/26 13:35:28 by saboulal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,44 @@ void ini_map(t_map *map,int argc)
      map->i = 0;
      map->k = 0;
     
+}
+
+void	map_game_full(t_map map)
+{
+	int	row;
+	int	col;
+
+	row = 0;
+	while (map.map1[row])
+	{
+		col = 0;
+		if (found_wall(map.map1, row) == 1)
+			ft_error_wall();
+		while (map.map1[row][col])
+		{
+			map_checks_(map, row, col);
+			col++;
+		}
+		row++;
+	}
+}
+
+void	map_checks_(t_map map, int r, int c)
+{
+	if ((r == 0 && space_waall(map.map1, r, c))
+		|| (r == map.r - 1 && space_waall(map.map1, r, c)))
+		ft_error_wall();
+	else if (c == 0 && space_waall(map.map1, r, c))
+		ft_error_wall();
+	else if (c == ft_strlen(map.map1[r]) - 1
+		&& space_waall(map.map1, r, c))
+		ft_error_wall();
+	else if ((r > 0 && r < map.r - 1)
+		&& (c > 0 && c < ft_strlen(map.map1[r]) - 1))
+	{
+		if (map.map1[r][c] == '0'
+			&& (map.map1[r + 1][c] == ' ' || map.map1[r - 1][c] == ' '
+			|| map.map1[r][c + 1] == ' ' || map.map1[r][c - 1] == ' '))
+			ft_error_wall();
+	}
 }
